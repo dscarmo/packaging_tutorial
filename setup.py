5fada22322691d0f2a587e83acff942042fc06f7
dscarmo <carmodiedre@outlook.com>
2020-05-21 16:41:47 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-dscarmo", # Replace with your own username
+    version="0.0.1",
+    author="Diedre Carmo",
+    author_email="diedre@dca.fee.unicamp.br",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dscarmo/packaging_tutorial",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Ubuntu 18.04",
+    ],
+    python_requires='>=3.6',
+)
